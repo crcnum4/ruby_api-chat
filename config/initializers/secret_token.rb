edfcec34081c648f1d5193291ad01d6e9e8e5152
Zeck <tools@3csonline.com>
2017-02-16 07:57:39 +0000

    initial commit app creatio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Chat::Application.config.secret_token = '64100509e2818d858bd5023ba7b3a725054d54487d38f9786cf9c0eae775d725b26fda237f643627d4c33f5170c98e682a898f2e5b1040a062c617930ef26927'
